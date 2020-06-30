f6ba99c6af680b2578922235e841e1309e69aa05
chenyanxi <chenyanxiself@163.com>
2020-06-30 14:12:19 +0800

    'first'

---
+++ auth_const.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2020/6/30 10:37
+# @Author  : yxChen
+
+
+list_test=[
+    {
+        'id':1,
+        'name':'社交大数据平台',
+        'children':[
+            {
+                'id':2,
+                'name':'基础数据',
+                'children':[
+                    {
+                        'id':3,
+                        'name':'导入数据任务',
+                        'api':'http://12121',
+                        'method':'post',
+                        'params':'',
+                        'body':'',
+                    }
+                ]
+            }
+        ]
+    }
+]
