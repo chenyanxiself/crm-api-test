0f06300622a9d2aabf1c89ebe40f25b9981843ad
chenyanxi <chenyanxiself@163.com>
2020-07-03 10:44:42 +0800

    fix

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ class Main:
         # session_tester = requests.Session()
         # self.session_tester: requests.Session = Login(session_tester, Config.test_user).get_login_session()
         self.write_header()
+
         s = requests.Session()
         s.headers.setdefault('Admin-Token', 'd6a54dd39f334172b351b98b502cb6d2')
         s.headers.setdefault('Authorization', 'd6a54dd39f334172b351b98b502cb6d2')
@@ -50,8 +51,18 @@ class Main:
                     raise SystemError('设置权限失败')
                 res_json=self.do_request(i)
                 if res_json['code'] != 403:
-                    print(res_json)
+                    rows = [
+                        {'接口名': i['name'], '接口id': i['id'], '接口url': i['api'],
+                         '接口所有内容': str(i), '返回状态码': res_json['code'], '返回内容': res_json, '测试是否通过': False, '期望值': '无权限',
+                         '实际值': '有权限'}]
+                    self.write_row(rows)
                     print(f'权限校验失败,target:无权限,now:有权限. {i}')
+                else:
+                    rows = [
+                        {'接口名': i['name'], '接口id': i['id'], '接口url': i['api'],
+                         '接口所有内容': str(i), '返回状态码': res_json['code'], '返回内容': res_json, '测试是否通过': True, '期望值': '无权限',
+                         '实际值': '无权限'}]
+                    self.write_row(rows)
                 new_auth_list = copy.deepcopy(auth_list)
                 new_auth_list.append(i['id'])
                 data = {"roleId": Config.test_user['role_id'], "permissionIds": new_auth_list,
@@ -62,8 +73,18 @@ class Main:
                     raise SystemError('设置权限失败')
                 res_json=self.do_request(i)
                 if res_json['code'] == 403:
-                    print(res_json)
+                    rows = [
+                        {'接口名': i['name'], '接口id': i['id'], '接口url': i['api'],
+                         '接口所有内容': str(i), '返回状态码': res_json['code'], '返回内容': res_json, '测试是否通过': False, '期望值': '有权限',
+                         '实际值': '无权限'}]
+                    self.write_row(rows)
                     print(f'权限校验失败,target:有权限,now:无权限. {i}')
+                else:
+                    rows = [
+                        {'接口名': i['name'], '接口id': i['id'], '接口url': i['api'],
+                         '接口所有内容': str(i), '返回状态码': res_json['code'], '返回内容': res_json, '测试是否通过': True, '期望值': '有权限',
+                         '实际值': '有权限'}]
+                    self.write_row(rows)
 
     def customer_business(self,item,auth_list:list):
         for i in item:
@@ -114,7 +135,6 @@ class Main:
                          '接口所有内容': str(i), '返回状态码': res_json['code'], '返回内容': res_json, '测试是否通过': False, '期望值': '有权限',
                          '实际值': '无权限'}]
                     self.write_row(rows)
-                    # print(res_json)
                     print(f'权限校验失败,target:有权限,now:无权限. {i}')
                 else:
                     rows = [
@@ -143,14 +163,16 @@ class Main:
             return res_json
         except:
             return {'code':500}
+
     def write_header(self):
         self.csvs = open(f'{str(int(time.time()))}.csv','w',newline='')
         headers: list = ['接口名', '接口id', '接口url', '接口所有内容', '返回状态码', '返回内容', '测试是否通过', '期望值', '实际值']
         self.writes = csv.DictWriter(self.csvs, headers)
-        self.xxxx = self.writes.writeheader()
-    def write_row(self,rows:list):
+        self.writes.writeheader()
 
-        self.xxxx = self.writes.writerows(rows)
+    def write_row(self,rows:list):
+        self.writes.writerows(rows)
+
     def clean(self):
         self.csvs.close()
 
@@ -158,19 +180,19 @@ class Main:
 
 
 if __name__ == '__main__':
-    # Main().start()
-    t = requests.Session()
-    t.headers.setdefault('Admin-Token', 'cbe8f393c9b7434fa658c4c6f3643bfd')
-    args = {
-        'method': 'GET',
-        'url': Config.base_host + '/social-api/api/channel/tagCategory/edit',
-        'params': {},
-        'json': {"id": "1273444722643361794", "name": "test", "children": [
-                    {"id": "1274222560149561346", "name": "kevin", "type": 2, "categoryId": "1273444722643361794"},
-                    {"id": "1277438167582040065", "name": "1", "type": 2, "categoryId": "1273444722643361794"},
-                    {"id": "1277896932286586882", "name": "test", "type": 2, "categoryId": "1273444722643361794"}],
-                         "type": 1}
-    }
-
-    res = t.request(**args)
-    print(res.text)
+    Main().start()
+    # t = requests.Session()
+    # t.headers.setdefault('Admin-Token', 'cbe8f393c9b7434fa658c4c6f3643bfd')
+    # args = {
+    #     'method': 'GET',
+    #     'url': Config.base_host + '/social-api/api/channel/tagCategory/edit',
+    #     'params': {},
+    #     'json': {"id": "1273444722643361794", "name": "test", "children": [
+    #                 {"id": "1274222560149561346", "name": "kevin", "type": 2, "categoryId": "1273444722643361794"},
+    #                 {"id": "1277438167582040065", "name": "1", "type": 2, "categoryId": "1273444722643361794"},
+    #                 {"id": "1277896932286586882", "name": "test", "type": 2, "categoryId": "1273444722643361794"}],
+    #                      "type": 1}
+    # }
+    #
+    # res = t.request(**args)
+    # print(res.text)
